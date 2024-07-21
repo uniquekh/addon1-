e7a2ecced6ce714427a8ed4c4d231ad1d5e05154
Unique <mujeem000@gmail.com>
2024-07-21 21:17:23 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -177,7 +177,7 @@ async def download_video(url,cmd, name):   #Bot Created by @NtrRazYt
         return os.path.isfile.splitext[0] + "." + "mp4"   #Bot Created by @NtrRazYt
    #Bot Created by @NtrRazYt
 async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):   #Bot Created by @NtrRazYt
-    reply = await m.reply_text(f"f"**⚡⚡⚡ᴜᴘʟᴏᴀᴅɪɴɢ ...**\n**📚❰Name❱**-`{name}` \n\n **ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ [😎𝖘cᾰ𝗺𝗺ⲉ𝗿:)™]**")   #Bot Created by @NtrRazYt
+    reply = await m.reply_text(f"**⚡⚡⚡ᴜᴘʟᴏᴀᴅɪɴɢ ...**\n**📚❰Name❱**-`{name}` \n\n **ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ [😎𝖘cᾰ𝗺𝗺ⲉ𝗿:)™]**")   #Bot Created by @NtrRazYt
     time.sleep(1)   #Bot Created by @NtrRazYt
     start_time = time.time()   #Bot Created by @NtrRazYt
     await m.reply_document(ka,caption=cc1)   #Bot Created by @NtrRazYt
